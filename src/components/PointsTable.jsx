cdf6bb0edc3a1884358603e8d1007872cb44eea1
pankaj297 <pankajnaik297@gmail.com>
2024-12-15 23:40:48 +0530

    MPL Points Table add

--- src/components/PointsTable.jsx
+++ src/components/PointsTable.jsx
@@ -6,7 +6,9 @@ export const PointsTable = () => {
   const [teams, setTeams] = useState([
     {
       id: 1,
+      res: "Semi Finalist",
       name: "Vishwanath Warriors",
+      logo: "./images/logo2.jpeg",
       played: 6,
       won: 4,
       lost: 2,
@@ -17,7 +19,9 @@ export const PointsTable = () => {
     },
     {
       id: 2,
+      res: "Runners Up",
       name: "Dipak Warriors",
+      logo: "./images/logo3.jpg",
       played: 6,
       won: 4,
       lost: 1,
@@ -28,7 +32,9 @@ export const PointsTable = () => {
     },
     {
       id: 3,
+      res: "Final Winners",
       name: "Black Panthers",
+      logo: "./images/logo4.jpeg",
       played: 6,
       won: 3,
       lost: 2,
@@ -39,7 +45,9 @@ export const PointsTable = () => {
     },
     {
       id: 4,
+      res: "League stage",
       name: "Shree Yodha",
+      logo: "./images/logo5.jpeg",
       played: 6,
       won: 2,
       lost: 4,
@@ -50,18 +58,22 @@ export const PointsTable = () => {
     },
     {
       id: 5,
+      res: "Semi Finalist",
       name: "Vishnu Blaster",
+      logo: "../images/logo6.jpeg",
       played: 6,
       won: 1,
       lost: 4,
       tied: 1,
       noResult: 0,
       exp: "1.5",
-      points: 8,
+      points: 9,
     },
     {
       id: 6,
+      res: "League stage",
       name: "Jagan Super Strikers",
+      logo: "./images/logo7.jpeg",
       played: 6,
       won: 0,
       lost: 6,
@@ -85,7 +97,9 @@ export const PointsTable = () => {
         <thead>
           <tr>
             <th>Rank</th>
+            <th>Last Years Results</th>
             <th>Team Name</th>
+            <th>Logo</th>
             <th>Played</th>
             <th>Won</th>
             <th>Lost</th>
@@ -99,7 +113,11 @@ export const PointsTable = () => {
           {teams.map((team, index) => (
             <tr key={team.id}>
               <td>{index + 1}</td>
+              <td>{team.res}</td>
               <td>{team.name}</td>
+              <td>
+                <img src={team.logo} alt="logos" />
+              </td>
               <td>{team.played}</td>
               <td>{team.won}</td>
               <td>{team.lost}</td>
