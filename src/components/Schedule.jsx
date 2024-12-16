17b059e5214459b433ad033d7685814b5a986992
pankaj297 <pankajnaik297@gmail.com>
2024-12-16 22:47:07 +0530

    schdule manage

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -2,78 +2,102 @@ import React from "react";
 import "./design/Schedule.css";
 
 export const Schedule = () => {
-  const matches = [
-    {
-      id: 1,
-      teamA: {
-        name: "Dipak Warriors",
-        logo: "./images/logo3.jpg",
-      },
-      teamB: {
-        name: "Malkheda Panthers",
-        logo: "./images/logo4.jpeg",
-      },
-    },
-    {
-      id: 2,
-      teamA: {
-        name: "Vishwanath Warriors",
-        logo: "./images/logo2.jpeg",
-      },
-      teamB: {
-        name: "Shree Yodha",
-        logo: "./images/logo5.jpeg",
-      },
-    },
-    {
-      id: 3,
-      teamA: {
-        name: "Vishnu Blaster",
-        logo: "./images/logo6.jpeg",
-      },
-      teamB: {
-        name: "Jagan Super Strikers",
-        logo: "./images/logo7.jpeg",
-      },
-    },
-    {
-      id: 4,
-      teamA: {
-        name: "Dipak Warriors",
-        logo: "./images/logo3.jpg",
-      },
-      teamB: {
-        name: "Malkheda Panthers",
-        logo: "./images/logo4.jpeg",
-      },
-    },
-    {
-      id: 5,
-      teamA: {
-        name: "Vishwanath Warriors",
-        logo: "./images/logo2.jpeg",
-      },
-      teamB: {
-        name: "Jagan Super Strikers",
-        logo: "./images/logo7.jpeg",
-      },
-    },
-    {
-      id: 6,
-      teamA: {
-        name: "Vishnu Blaster",
-        logo: "./images/logo6.jpeg",
-      },
-      teamB: {
-        name: "Shree Yodha",
-        logo: "./images/logo5.jpeg",
-      },
-    },
-  ];
+  
+const matches = [
+  // Day 1
+  {
+    id: 1,
+    teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
+    teamB: { name: "Malkheda Panthers", logo: "./images/logo4.jpeg" },
+  },
+  {
+    id: 2,
+    teamA: { name: "Shree Yodha", logo: "./images/logo5.jpeg" },
+    teamB: { name: "Jagan Super Strikers", logo: "./images/logo7.jpeg" },
+  },
+  {
+    id: 3,
+    teamA: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
+    teamB: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
+  },
+
+  // Day 2
+  {
+    id: 4,
+    teamA: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
+    teamB: { name: "Jagan Super Strikers", logo: "./images/logo7.jpeg" },
+  },
+  {
+    id: 5,
+    teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
+    teamB: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
+  },
+  {
+    id: 6,
+    teamA: { name: "Malkheda Panthers", logo: "./images/logo4.jpeg" },
+    teamB: { name: "Shree Yodha", logo: "./images/logo5.jpeg" },
+  },
+
+  // Day 3
+
+  {
+    id: 7,
+    teamA: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
+    teamB: { name: "Shree Yodha", logo: "./images/logo5.jpeg" },
+  },
+  {
+    id: 8,
+    teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
+    teamB: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
+  },
+  {
+    id: 9,
+    teamA: { name: "Malkheda Panthers", logo: "./images/logo4.jpeg" },
+    teamB: { name: "Jagan Super Strikers", logo: "./images/logo7.jpeg" },
+  },
+
+  // Day 4
+
+  {
+    id: 10,
+    teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
+    teamB: { name: "Shree Yodha", logo: "./images/logo5.jpeg" },
+  },
+
+  {
+    id: 11,
+    teamA: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
+    teamB: { name: "Jagan Super Strikers", logo: "./images/logo7.jpeg" },
+  },
+  {
+    id: 12,
+    teamA: { name: "Malkheda Panthers", logo: "./images/logo4.jpeg" },
+    teamB: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
+  },
+
+  // Day 5
+
+  {
+    id: 13,
+    teamA: { name: "Malkheda Panthers", logo: "./images/logo4.jpeg" },
+    teamB: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
+  },
+  {
+    id: 14,
+    teamA: { name: "Shree Yodha", logo: "./images/logo5.jpeg" },
+    teamB: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
+  },
+  {
+    id: 13,
+    teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
+    teamB: { name: "Jagan Super Strikers", logo: "./images/logo7.jpeg" },
+  },
+];
+
 
   return (
     <div className="schedule-container">
-      <h2>Demo Schedule Upcoming Cricket Matches</h2>
+      <h2>Schedule Upcoming Cricket Matches</h2>
       <div className="matchess">
         {matches.map((match) => (
           <div key={match.id} className="match-cards">
