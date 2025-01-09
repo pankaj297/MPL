62a6e2cb794decf4ec146d49ce56b6636e4c57f5
pankaj297 <pankajnaik297@gmail.com>
2025-01-09 18:43:45 +0530

    Update Day 3 points table

--- src/components/ShowUpdate.jsx
+++ src/components/ShowUpdate.jsx
@@ -13,17 +13,11 @@ export const ShowUpdate = () => {
           resolve([
             {
               id: 1,
-              massage: `1st Match Dipak Warriors Vs Malkheda Panthers 🏏 Result : Dipak Warriors won by 29 Run 🥳`,
+              massage: `🎉Final 14 Jan 11Am🏏🥳`,
             },
             {
               id: 2,
-              massage:
-                "2nd Match Shree Yodha Vs Jagan Super Strikers 🏏 Result : Jagan Super Strikers won by 48 Run 🥳",
-            },
-            {
-              id: 3,
-              massage:
-                "3rd Match Vishwanath Warriors Vs Vishnu Blaster 🏏 Result : Vishnu Blaster won by 44 Run 🥳",
+              massage: `🎉Semi Finals 12 Jan 11Am Start🏏🥳`,
             },
           ]);
         }, 1000); // Simulating network delay
