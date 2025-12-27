0a6bdd427a339de53afddb8e3d34a72a431bc708
pankaj297 <pankajnaik297@gmail.com>
2025-12-27 19:44:36 +0530

    Match date change

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -6,113 +6,113 @@ export const Schedule = () => {
     {
       id: 1,
       day: 1,
-      date: "07/01/2026",
+      date: "06/01/2026",
       teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
       teamB: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
     },
     {
       id: 2,
       day: 1,
-      date: "07/01/2026",
+      date: "06/01/2026",
       teamA: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
       teamB: { name: "Jijau Fighter", logo: "./images/logoJijau.jpeg" },
     },
     {
       id: 3,
       day: 1,
-      date: "07/01/2026",
+      date: "06/01/2026",
       teamA: { name: "Mata Nagar", logo: "./images/logo8.jpeg" },
       teamB: { name: "Ram Rajya Pratishthan", logo: "./images/logo4.png" },
     },
 
-    // Day 2 - 08/01/2026
+    // Day 2 - 07/01/2026
     {
       id: 4,
       day: 2,
-      date: "08/01/2026",
+      date: "07/01/2026",
       teamA: { name: "Jijau Fighter", logo: "./images/logoJijau.jpeg" },
       teamB: { name: "Ram Rajya Pratishthan", logo: "./images/logo4.png" },
     },
     {
       id: 5,
       day: 2,
-      date: "08/01/2026",
+      date: "07/01/2026",
       teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
       teamB: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
     },
     {
       id: 6,
       day: 2,
-      date: "08/01/2026",
+      date: "07/01/2026",
       teamA: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
       teamB: { name: "Mata Nagar", logo: "./images/logo8.jpeg" },
     },
 
-    // Day 3 - 09/01/2026
+    // Day 3 - 08/01/2026
     {
       id: 7,
       day: 3,
-      date: "09/01/2026",
+      date: "08/01/2026",
       teamA: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
       teamB: { name: "Mata Nagar", logo: "./images/logo8.jpeg" },
     },
     {
       id: 8,
       day: 3,
-      date: "09/01/2026",
+      date: "08/01/2026",
       teamA: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
       teamB: { name: "Ram Rajya Pratishthan", logo: "./images/logo4.png" },
     },
     {
       id: 9,
       day: 3,
-      date: "09/01/2026",
+      date: "08/01/2026",
       teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
       teamB: { name: "Jijau Fighter", logo: "./images/logoJijau.jpeg" },
     },
 
-    // Day 4 - 10/01/2026
+    // Day 4 - 09/01/2026
     {
       id: 10,
       day: 4,
-      date: "10/01/2026",
+      date: "09/01/2026",
       teamA: { name: "Jijau Fighter", logo: "./images/logoJijau.jpeg" },
       teamB: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
     },
     {
       id: 11,
       day: 4,
-      date: "10/01/2026",
+      date: "09/01/2026",
       teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
       teamB: { name: "Mata Nagar", logo: "./images/logo8.jpeg" },
     },
     {
       id: 12,
       day: 4,
-      date: "10/01/2026",
+      date: "09/01/2026",
       teamA: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
       teamB: { name: "Ram Rajya Pratishthan", logo: "./images/logo4.png" },
     },
 
-    // Day 5 - 11/01/2026
+    // Day 5 - 10/01/2026
     {
       id: 13,
       day: 5,
-      date: "11/01/2026",
+      date: "10/01/2026",
       teamA: { name: "Jijau Fighter", logo: "./images/logoJijau.jpeg" },
       teamB: { name: "Mata Nagar", logo: "./images/logo8.jpeg" },
     },
     {
       id: 14,
       day: 5,
-      date: "11/01/2026",
+      date: "10/01/2026",
       teamA: { name: "Vishwanath Warriors", logo: "./images/logo2.jpeg" },
       teamB: { name: "Vishnu Blaster", logo: "./images/logo6.jpeg" },
     },
     {
       id: 15,
       day: 5,
-      date: "11/01/2026",
+      date: "10/01/2026",
       teamA: { name: "Dipak Warriors", logo: "./images/logo3.jpg" },
       teamB: { name: "Ram Rajya Pratishthan", logo: "./images/logo4.png" },
     },
@@ -121,7 +121,7 @@ export const Schedule = () => {
     {
       id: 16,
       day: 6,
-      date: "12/01/2026",
+      date: "11/01/2026",
       teamA: {
         name: "TBC (Qualify 1)",
         logo: "./images/logo1.png",
@@ -134,7 +134,7 @@ export const Schedule = () => {
     {
       id: 17,
       day: 6,
-      date: "12/01/2026",
+      date: "11/01/2026",
       teamA: {
         name: "TBC (Qualify 2)",
         logo: "./images/logo1.png",
@@ -145,8 +145,8 @@ export const Schedule = () => {
     // Third Place - Day 7
     {
       id: 18,
-      day: 7,
-      date: "12/01/2026",
+      day: 6,
+      date: "11/01/2026",
       teamA: { name: "Semi Final Loser 1", logo: "./images/logo1.png" },
       teamB: { name: "Semi Final Loser 2", logo: "./images/logo1.png" },
     },
@@ -154,8 +154,8 @@ export const Schedule = () => {
     // Final - Day 8
     {
       id: 19,
-      day: 8,
-      date: "12/01/2026",
+      day: 6,
+      date: "11/01/2026",
       teamA: { name: "Finalist 1", logo: "./images/logo1.png" },
       teamB: { name: "Finalist 2", logo: "./images/logo1.png" },
     },
